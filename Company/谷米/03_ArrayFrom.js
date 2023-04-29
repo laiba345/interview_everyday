d8471a82deed0cd3e3f55158303effd33021bb4e
Kang Huang <kangder@KangdeMBP.lan>
2023-04-29 11:01:52 +0800

    update

--- Company/谷米/03_ArrayFrom.js
+++ Company/谷米/03_ArrayFrom.js
@@ -1,10 +1,16 @@
 let arr = Array.from([1, 2, 3], (x) => x * 2);
 console.log(arr); // [ 2, 4, 6 ]
 
-let arr2 = Array.from({ length: 10 }, (_, i) => i * i);
+// 下方的Array.from() 表示生成一个数组，其中该数组的长度为10，
+// 后面的i是从0-9开始计算的；这一点很关键；
+let arr2 = Array.from({ length: 10 }, (_, i) => i + 1);
 console.log(arr2);
 
+/* 
+    注意点
+    - 函数参数列表中的 _ ;表示一个占位符没有别的意思
+*/
 // 因为前面设定{length: 10}，后面中的i是从0开始计数的
-Array.from({ length: 10 }, (_, i) =>
-  fetch(`/api/order/${i + 1}`).then((res) => res.json())
-);
+// Array.from({ length: 10 }, (_, i) =>
+//   fetch(`/api/order/${i + 1}`).then((res) => res.json())
+// );
