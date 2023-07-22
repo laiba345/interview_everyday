ae42017077f3242899954ec3b77d1a4a862bd8d1
KK <2846016855@qq.com>
2023-07-22 09:29:53 +0800

    0722-浅拷贝vs深拷贝

---
+++ Everyday/0722/浅拷贝.js
@@ -0,0 +1,21 @@
+// 浅拷贝示例
+const originalObj = {
+    name: 'John',
+    age: 30,
+    hobbies: ['reading', 'swimming'],
+};
+
+// 使用Object.assign()进行浅拷贝
+const shallowCopyObj = Object.assign({}, originalObj);
+
+// 修改浅拷贝对象的属性
+shallowCopyObj.name = 'Jane';
+shallowCopyObj.hobbies.push('running');
+
+console.log(originalObj); // { name: 'John', age: 30, hobbies: ['reading', 'swimming', 'running'] }
+console.log(shallowCopyObj); // { name: 'Jane', age: 30, hobbies: ['reading', 'swimming', 'running'] }
+
+// 解释
+// 虽然修改了浅拷贝对象的name属性，但原始对象的name属性没有受到影响。
+// 但是，由于浅拷贝只复制了引用，
+// 当修改浅拷贝对象中的引用类型属性（如数组hobbies）时，原始对象也会受到影响。
---
+++ Everyday/0722/深拷贝.js
@@ -0,0 +1,19 @@
+// 深拷贝示例
+const originalObj = {
+    name: 'John',
+    age: 30,
+    hobbies: ['reading', 'swimming'],
+};
+
+// 使用JSON.parse()和JSON.stringify()进行深拷贝
+const deepCopyObj = JSON.parse(JSON.stringify(originalObj));
+
+// 修改深拷贝对象的属性
+deepCopyObj.name = 'Jane';
+deepCopyObj.hobbies.push('running');
+
+console.log(originalObj); // { name: 'John', age: 30, hobbies: ['reading', 'swimming'] }
+console.log(deepCopyObj); // { name: 'Jane', age: 30, hobbies: ['reading', 'swimming', 'running'] }
+
+// 解释
+// 进行深拷贝以后的新的对象与原始对象完全独立，对新对象的修改不会影响原始对象
