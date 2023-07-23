3b5e4b61796abed0266abbff490c5ba354133b26
KK <2846016855@qq.com>
2023-07-23 08:37:40 +0800

    0723

--- Everyday/0722/浅拷贝.js
+++ Everyday/0722/浅拷贝.js
@@ -8,7 +8,7 @@ const originalObj = {
 // 使用Object.assign()进行浅拷贝
 const shallowCopyObj = Object.assign({}, originalObj);
 
-// 修改浅拷贝对象的属性
+// 修改浅拷贝对象的属性  
 shallowCopyObj.name = 'Jane';
 shallowCopyObj.hobbies.push('running');
 
@@ -19,3 +19,5 @@ console.log(shallowCopyObj); // { name: 'Jane', age: 30, hobbies: ['reading', 's
 // 虽然修改了浅拷贝对象的name属性，但原始对象的name属性没有受到影响。
 // 但是，由于浅拷贝只复制了引用，
 // 当修改浅拷贝对象中的引用类型属性（如数组hobbies）时，原始对象也会受到影响。
+// name属性为普通属性,但是数组为引用类型,所以对其修改,会影响原始数组
+
--- Everyday/0722/深拷贝.js
+++ Everyday/0722/深拷贝.js
@@ -16,4 +16,7 @@ console.log(originalObj); // { name: 'John', age: 30, hobbies: ['reading', 'swim
 console.log(deepCopyObj); // { name: 'Jane', age: 30, hobbies: ['reading', 'swimming', 'running'] }
 
 // 解释
-// 进行深拷贝以后的新的对象与原始对象完全独立，对新对象的修改不会影响原始对象
+// 进行深拷贝以后的新的对象与原始对象完全独立，
+// 对新对象的修改不会影响原始对象
+
+// 深度拷贝生成的新的对象就是和原始对象完全独立的
