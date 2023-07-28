a7205402f5777cdb9cd8aa137be5da302002480e
laiba345 <2846016855@qq.com>
2023-07-28 09:53:19 +0800

    0728

--- Everyday/0717/forIn.js
+++ Everyday/0717/forIn.js
@@ -3,3 +3,9 @@ for (let prop in obj) {
   console.log(prop); // 输出属性名：a, b, c
   console.log(obj[prop]); // 输出属性值：1, 2, 3
 }
+
+// const arr = [1, 2, 3, 4, 5]
+// for(let item in arr) {
+//   console.log(item); // 可以遍历出来索引值
+//   console.log(arr[item]); // 拿到具体的元素
+// }
